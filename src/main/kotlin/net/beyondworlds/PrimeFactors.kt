8b19913dd492d46206f764cad18e610f7bbd22d5
Dirk Rademann <dirk.rademann@lhind.dlh.de>
2017-07-18 12:52:12 +0200

    Initial commit with basic PrimeFactor Kata.

---
+++ src/main/kotlin/net/beyondworlds/PrimeFactors.kt
@@ -0,0 +1,15 @@
+package net.beyondworlds
+
+fun primeFactorsOf(n: Int): List<Int> {
+    var r = n
+    var factors = emptyList<Int>()
+    var c = 2
+    while (r > 1) {
+        while (r % c == 0) {
+            factors += c
+            r /= c
+        }
+        c++
+    }
+    return factors
+}
---
+++ src/test/kotlin/net/beyondworlds/PrimeFactorsTest.kt
@@ -0,0 +1,48 @@
+package net.beyondworlds
+
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class PrimeFactorsTest {
+
+    @Test
+    fun one() {
+        assertEquals(expected = emptyList(), actual = primeFactorsOf(1))
+    }
+
+    @Test
+    fun two() {
+        assertEquals(listOf(2), primeFactorsOf(2))
+    }
+
+    @Test
+    fun three() {
+        assertEquals(listOf(3), primeFactorsOf(3))
+    }
+
+    @Test
+    fun four() {
+        assertEquals(listOf(2, 2), primeFactorsOf(4))
+    }
+
+    @Test
+    fun five() {
+        assertEquals(listOf(5), primeFactorsOf(5))
+    }
+
+    @Test
+    fun six() {
+        assertEquals(listOf(2, 3), primeFactorsOf(6))
+    }
+
+    @Test
+    fun eight() {
+        assertEquals(listOf(2, 2, 2), primeFactorsOf(8))
+    }
+
+    @Test
+    fun nine() {
+        assertEquals(listOf(3, 3), primeFactorsOf(9))
+    }
+
+}
