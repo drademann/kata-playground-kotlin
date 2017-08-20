7975cb2c25136329c1eb357a836b9bf873945cd2
Dirk Rademann <dirk.rademann@icloud.com>
2017-08-20 11:55:58 +0200

    Makes use of Kotlin's Stdlib .split and the spread operator.
    Updates Kotlin version to 1.1.4-2

--- src/main/kotlin/kata/stringcalculator/StringCalculator.kt
+++ src/main/kotlin/kata/stringcalculator/StringCalculator.kt
@@ -2,13 +2,13 @@ package kata.stringcalculator
 
 fun String.sum(): Int = if (isEmpty()) 0 else StringCalculator(this).sum()
 
-class StringCalculator(private val input: String, private val delimiters: String = ",\n") {
+class StringCalculator(private val input: String, private val delimiters: CharArray = charArrayOf(',', '\n')) {
 
     fun sum(): Int =
             if (hasCustomDelimiter())
-                StringCalculator(remainingInput(), delimitersAppendedWith(customDelimiter())).sum()
+                StringCalculator(remainingInput(), charArrayOf(customDelimiter(), *delimiters)).sum()
             else
-                input.split(delimitersRegex())
+                input.split(*delimiters)
                         .map { it.toInt() }
                         .filter { it <= 1000 }
                         .map { if (it < 0) throw IllegalArgumentException() else it }
@@ -16,14 +16,8 @@ class StringCalculator(private val input: String, private val delimiters: String
 
     private fun hasCustomDelimiter() = input.startsWith("//")
 
-    private fun delimitersAppendedWith(customDelimiter: Char): String {
-        return ",\n$customDelimiter"
-    }
-
-    private fun remainingInput() = input.substring(4)
-
     private fun customDelimiter() = input[2]
 
-    private fun delimitersRegex() = Regex("[$delimiters]")
+    private fun remainingInput() = input.substring(4)
 
 }
