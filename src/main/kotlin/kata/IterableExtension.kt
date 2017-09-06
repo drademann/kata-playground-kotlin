454149a43767bb916a15780f479848c23d85b229
Dirk Rademann <dirk.rademann@icloud.com>
2017-09-06 19:45:16 +0200

    Shows use of sequences.

--- src/main/kotlin/kata/IterableExtension.kt
+++
@@ -1,6 +0,0 @@
-package kata
-
-fun <T> Iterable<T>.check(forException: () -> Exception = ::IllegalArgumentException, check: (T) -> Boolean): Iterable<T> {
-    if (!all(check)) throw forException.invoke()
-    return this
-}
--- src/main/kotlin/kata/stringcalculator/StringCalculator.kt
+++ src/main/kotlin/kata/stringcalculator/StringCalculator.kt
@@ -1,7 +1,5 @@
 package kata.stringcalculator
 
-import kata.check
-
 fun String?.sum(): Int = if (this == null) -1 else sumOf(this)
 
 fun sumOf(input: String?) = when {
@@ -17,7 +15,7 @@ private class StringCalculator(private val input: String,
             if (hasCustomDelimiter())
                 StringCalculator(inputWithoutCustomDelimiter, delimiters.plus(customDelimiter)).sum()
             else
-                input.split(*delimiters)
+                input.split(*delimiters).asSequence()
                         .map { it.toInt() }
                         .check { it >= 0 }
                         .filter { it <= 1000 }
@@ -31,4 +29,25 @@ private class StringCalculator(private val input: String,
     private inline val customDelimiter
         get() = input[2]
 
+}
+
+fun <T> Iterable<T>.check(forException: () -> Exception = ::IllegalArgumentException, check: (T) -> Boolean): Iterable<T> {
+    if (!all(check)) throw forException.invoke()
+    return this
+}
+
+fun <T> Sequence<T>.check(forException: () -> Exception = ::IllegalArgumentException, check: (T) -> Boolean): Sequence<T> {
+    return object : Sequence<T> {
+        override fun iterator(): Iterator<T> = object : Iterator<T> {
+            val iterator = this@check.iterator()
+
+            override fun next(): T =
+                    iterator.next().apply {
+                        if (!check(this)) throw forException.invoke()
+                    }
+
+            override fun hasNext(): Boolean = iterator.hasNext()
+
+        }
+    }
 }
