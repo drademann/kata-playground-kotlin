9255b9c1943a1dbd140644f7f755674fc3f1d327
Dirk Rademann <dirk.rademann@lhind.dlh.de>
2017-09-12 11:21:32 +0200

    Cleans code.

--- src/main/kotlin/kata/wordchain/WordChains.kt
+++ src/main/kotlin/kata/wordchain/WordChains.kt
@@ -18,25 +18,26 @@ class Words(private vararg val vocabulary: String) {
     private fun differentLength(from: String, to: String) = from.length != to.length
     private fun unknown(word: String) = !vocabulary.contains(word)
 
-    private fun search(chain: Chain, to: String): Chain {
-        val candidates = vocabulary
-                .filter { it.length == to.length }
-                .filter { differentCharsIn(it, chain.last) == 1 }
-                .filterNot { chain.contains(it) }
-        val chains = mutableListOf<Chain>()
-        for (candidate in candidates) {
-            if (candidate == to) {
-                val possibleChain = chain.plus(candidate)
-                if (possibleChain.isNotEmpty())
-                    chains.add(possibleChain)
-            }
-            chains.add(search(chain.plus(candidate), to))
-        }
-        return chains
-                .filter { it.isNotEmpty() }
-                .minBy { it.length }
-                ?: NoChain
-    }
+    private fun search(chain: Chain, to: String) =
+            nextChains(chain, to)
+                    .filter { it.isNotEmpty() }
+                    .minBy { it.length } ?: NoChain
+
+    private fun nextChains(chain: Chain, to: String): Sequence<Chain> =
+            nextCandidates(chain, to)
+                    .map { candidate ->
+                        val candidateChain = chain.plus(candidate)
+                        if (candidate == to)
+                            candidateChain
+                        else
+                            search(candidateChain, to)
+                    }
+
+    private fun nextCandidates(chain: Chain, to: String) =
+            vocabulary.asSequence()
+                    .filter { it.length == to.length }
+                    .filter { differentCharsIn(it, chain.last) == 1 }
+                    .filterNot { chain.contains(it) }
 
     private fun differentCharsIn(s1: String, s2: String) = s1.mapIndexed { index, c -> if (c == s2[index]) 0 else 1 }.sum()
 
