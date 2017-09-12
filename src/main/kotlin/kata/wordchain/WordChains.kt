4d56879acfff3b69e0b83d7098c6ae0eaadd4ac1
Dirk Rademann <dirk.rademann@lhind.dlh.de>
2017-09-12 10:59:35 +0200

    Replaces use of Array<String> with explicit Chain class.

--- src/main/kotlin/kata/wordchain/WordChains.kt
+++ src/main/kotlin/kata/wordchain/WordChains.kt
@@ -2,22 +2,28 @@ package kata.wordchain
 
 class Words(private vararg val vocabulary: String) {
 
-    fun findChain(from: String, to: String) =
-            if (unableToChain(from, to))
-                emptyArray()
-            else
-                search(arrayOf(from), to)
+    fun findChain(from: String, to: String) = find(from, to).asArray()
 
-    private fun unableToChain(from: String, to: String) = from.isEmpty() || to.isEmpty() || differentLength(from, to) || unknown(from) || unknown(to)
+    private fun find(from: String, to: String) =
+            if (unableToChain(from, to))
+                NoChain
+            else
+                search(Chain(from), to)
+
+    private fun unableToChain(from: String, to: String) =
+            from.isEmpty() || to.isEmpty()
+                    || differentLength(from, to)
+                    || unknown(from) || unknown(to)
+
     private fun differentLength(from: String, to: String) = from.length != to.length
     private fun unknown(word: String) = !vocabulary.contains(word)
 
-    private fun search(chain: Array<String>, to: String): Array<String> {
+    private fun search(chain: Chain, to: String): Chain {
         val candidates = vocabulary
                 .filter { it.length == to.length }
-                .filter { differentCharsIn(it, chain.last()) == 1 }
+                .filter { differentCharsIn(it, chain.last) == 1 }
                 .filterNot { chain.contains(it) }
-        val chains = mutableListOf<Array<String>>()
+        val chains = mutableListOf<Chain>()
         for (candidate in candidates) {
             if (candidate == to) {
                 val possibleChain = chain.plus(candidate)
@@ -28,10 +34,33 @@ class Words(private vararg val vocabulary: String) {
         }
         return chains
                 .filter { it.isNotEmpty() }
-                .minBy { it.size }
-                ?: emptyArray()
+                .minBy { it.length }
+                ?: NoChain
     }
 
     private fun differentCharsIn(s1: String, s2: String) = s1.mapIndexed { index, c -> if (c == s2[index]) 0 else 1 }.sum()
 
 }
+
+private class Chain(private val elements: List<String>) {
+
+    constructor(startElement: String) : this(listOf(startElement))
+    constructor() : this(emptyList())
+
+    fun asArray(): Array<String> = elements.toTypedArray()
+
+    fun contains(element: String) = elements.contains(element)
+
+    fun isNotEmpty() = elements.isNotEmpty()
+
+    fun plus(element: String): Chain = Chain(elements.plus(element))
+
+    val length: Int
+        get() = elements.size
+
+    val last: String
+        get() = elements.last()
+
+}
+
+private val NoChain = Chain()
