b5da865c42b815cc4f29cda267c64af53fef970f
Dirk Rademann <dirk.rademann@icloud.com>
2018-06-24 22:13:00 +0200

    Update to Kotlin 1.2.50.

--- src/main/kotlin/kata/stringcalculator/StringCalculator.kt
+++ src/main/kotlin/kata/stringcalculator/StringCalculator.kt
@@ -2,30 +2,32 @@ package kata.stringcalculator
 
 import kata.check
 
-fun String?.sum(): Int = sumOf(this)
+fun String?.sum() = sumOf(this)
 
-fun sumOf(input: String?) = when {
-    input == null -> -1
-    input.isEmpty() -> 0
-    else -> StringCalculator(input).sum()
-}
+fun sumOf(input: String?): Int =
+        when {
+            input == null -> -1
+            input.isEmpty() -> 0
+            else -> StringCalculator(input).sum()
+        }
 
-private class StringCalculator(private val input: String,
-                               private val delimiters: CharArray = charArrayOf(',', '\n')) {
+class StringCalculator(private val input: String,
+                       private val delimiters: CharArray = charArrayOf(',', '\n')) {
 
     fun sum(): Int =
-            if (hasCustomDelimiter())
-                StringCalculator(inputWithoutCustomDelimiter, delimiters + customDelimiter).sum()
+            if (input.startsWith("//"))
+                StringCalculator(inputWithoutCustomDelimiter,
+                                 customDelimiters).sum()
             else
-                input.split(*delimiters).asSequence()
+                input.split(*delimiters)
                         .map { it.toInt() }
                         .check { it >= 0 }
                         .filter { it <= 1000 }
                         .sum()
 
-    private fun hasCustomDelimiter() = input.startsWith("//")
+    private val inputWithoutCustomDelimiter = input.substringAfter('\n')
 
-    private val inputWithoutCustomDelimiter get() = input.substringAfter('\n')
-    private val customDelimiter get() = input[2]
+    private val customDelimiters: CharArray
+        get() = charArrayOf(input.elementAt(2), '\n')
 
 }
